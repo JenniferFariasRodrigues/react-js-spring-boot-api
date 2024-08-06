66586ceb2287c3159da607de8fe007e08d727484
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-08-06 20:28:19 -0300

    feature:login screan

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./styles.css";
-
+import logo from "../../assets/logo.svg";
+import padlock from "../../assets/padlock.png";
+import logoImage from '../../assets/logo.svg'
 export default function Login() {
-  return <h1>Welcome</h1>;
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImage} alt="JenniferLogo" />
+        <form>
+          <h1>Access your account</h1>
+          <input placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit"></button>
+        </form>
+      </section>
+      <img src={padlock} alt="Login" />
+    </div>
+  );
 }
